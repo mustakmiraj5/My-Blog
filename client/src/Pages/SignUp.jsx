5574aeae06bc2757cc49377b2c4bd7da2701d810
mustakmiraj5 <wildmiraj@gmail.com>
2024-01-31 08:19:58 +0600

    Random

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignUp = () => {
+    const [formData, setFormData] = useState({})
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.id] : e.target.value})
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
     return (
         <div>
             <div className="hero min-h-screen bg-gray-900">
@@ -10,24 +17,24 @@ const SignUp = () => {
       <p className='py-6'>Discover stories, thinking, and expertise from writers on any topic.</p>
     </div>
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
+      <form className="card-body" onSubmit={handleSubmit}>
         <div className="form-control">
           <label className="label">
             <span className="label-text font-bold">Your Username</span>
           </label>
-          <input id='username' type="text" placeholder="username" className="input input-bordered input-warning w-full max-w-xs" required />
+          <input id='username' onChange={handleChange} type="text" placeholder="username" className="input input-bordered input-warning w-full max-w-xs" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text font-bold">Email</span>
           </label>
-          <input id='email' type="email" placeholder="email" className="input input-bordered input-warning w-full max-w-xs" required />
+          <input id='email' onChange={handleChange} type="email" placeholder="email" className="input input-bordered input-warning w-full max-w-xs" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text font-bold">Password</span>
           </label>
-          <input id='password' type="password" placeholder="password" className="input input-bordered input-warning w-full max-w-xs" required />
+          <input id='password' onChange={handleChange} type="password" placeholder="password" className="input input-bordered input-warning w-full max-w-xs" required />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
